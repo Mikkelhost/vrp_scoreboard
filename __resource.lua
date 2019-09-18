c2e94d8703da37aa958023a9babb50c281ff08c3
Mikkelhost <36157675+Mikkelhost@users.noreply.github.com>
2019-09-18 20:45:34 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,12 +1,18 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-description 'ESX Scoreboard'
+description 'VRP Scoreboard'
 
 version '1.0.0'
 
-server_script 'server/main.lua'
-
-client_script 'client/main.lua'
+client_scripts{
+    	"lib/Tunnel.lua",
+	"lib/Proxy.lua",
+    	"client/client.lua"
+}
+server_scripts{
+    	"@vrp/lib/utils.lua",
+    	"server/server.lua"
+}
 
 ui_page 'html/scoreboard.html'
 
